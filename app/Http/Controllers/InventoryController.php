73e91b5054c147c365db92f4beb8444c0d48cbce
maksymyurchenkosmarttek <maksym.yurchenko@smartteksas.com>
2025-07-31 12:56:43 +0300

    tooltip delete: hohfix

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Services\TooltipService;
 
 class InventoryController extends Controller
 {
@@ -23,11 +22,5 @@ class InventoryController extends Controller
         }
 
         return view('inventory', compact('character', 'inventory', 'equipped', 'equippedBySlot'));
-    }
-
-    public function showTooltipForItem($item)
-    {
-        $title = TooltipService::makeTitleFromItem($item);
-        $this->setHoveredItemTooltip($title);
     }
 }
